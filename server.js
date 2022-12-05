89a65fb4ff5df01ef0521c8e19649e72da4cbff5
Jason Tam <jasonyst93@gmail.com>
2022-12-05 14:16:27 +0800

    changing SSL

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true,
+        ssl: { rejectUnauthorized: false }
     }
 });
 
@@ -38,7 +38,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
     console.log(`App is running on ${process.env.PORT}`)
 })
 
